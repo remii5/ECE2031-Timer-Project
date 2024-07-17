01a5d61ca7a38aee9487571cce8caf7e6c1dd5dd
ecuas <ecuasonic@gmail.com>
2024-07-17 17:11:35 -0400

    Updated asm

--- NewTimerTest.asm
+++ NewTimerTest.asm
@@ -5,20 +5,20 @@ ORG 0
     ; --------------------
     ; Count from 0.0 to 5.0
     LOAD    ZERO
-    IN      Timer2
-LOOP1:
     OUT     Timer2
-    OUT     Hex0
+LOOP1:
+    IN      Timer2
+    OUT     Hex1
     SUB     FIVE
     JNEG    LOOP1
     ; --------------------
-    ; Count to 16.0 from 21.0
-    LOAD    SIXTEEN
-    IN      Timer2
-LOOP2:
+    ; Count to 2.5 from 7.5
+    LOAD    TWO_FIVE
     OUT     Timer2
+LOOP2:
+    IN      Timer2
     OUT     Hex0
-    SUB     TWENTY_ONE
+    SUB     SEVEN_FIVE
     JNEG    LOOP2
     ; --------------------
 
@@ -26,9 +26,12 @@ LOOP2:
 
 ; IO address constants
 ZERO:       DW  0
+TWO_FIVE:   DW  25
 FIVE:       DW  50
-SIXTEEN:    DW  160
-TWENTY_ONE: DW  210
+SEVEN_FIVE: DW  75
+
+; Variables
+TEMP:       DW  0
 
 Switches:   EQU 000
 LEDs:       EQU 001
